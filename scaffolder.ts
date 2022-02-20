11cf34065aa719f5b154972229d9ef6b0b3af560
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-02-20 21:06:48 +0000

    fix: Dirs work now

--- scaffolder.ts
+++ scaffolder.ts
@@ -1,9 +1,16 @@
 import { exists } from "https://deno.land/std/fs/mod.ts";
 
+const force = Deno.args.includes("--force");
+const settingsExists = await exists("settings.json");
+
 let vscodeSettings = true;
 
-if (await exists("./vscode/settings.json") && !Deno.args.includes("--force")) {
+if (settingsExists && !force) {
   vscodeSettings = false;
+} else if (settingsExists && force) {
+  await Deno.remove("./.vscode/settings.json");
+} else {
+  await Deno.mkdir("./.vscode", { recursive: true });
 }
 
 if (vscodeSettings) {
